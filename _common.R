e2f66f14493c80aa0059f42109165fd0759352f4
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-12-08 10:02:39 +0100

    fix code using rmd fragments

--- _common.R
+++ _common.R
@@ -1,9 +1,15 @@
+dir.create("rmd-fragments")
+
 get_vcr_doc <- function(path) {
+  fs::dir_create(dirname(file.path("rmd-fragments", path)), recurse = TRUE)
   fragment_api <- gh::gh(
     "/repos/:owner/:repo/contents/:path",
     owner = "ropensci",
     repo = "vcr",
     path = path
   )
-  cat(rawToChar(base64enc::base64decode(fragment_api$content)))
+  cat(
+    rawToChar(base64enc::base64decode(fragment_api$content)),
+    file = file.path("rmd-fragments", path)
+    )
 }
